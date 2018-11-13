86c8f405c1f1e692fcf56070278aec18bf94e8bc
Nilesh Saxena <dragonair2311@gmail.com>
2018-11-14 03:14:55 +0530

    With UI

--- www/js/index.js
+++ www/js/index.js
@@ -10,35 +10,34 @@ var NewD = new Date();
 var DayCode = NewD.getDay();
 if(DayCode>=1 && DayCode<=5)
 	document.getElementById(DayCode).style.fontSize = "30px";	
-//window.localStorage.clear();
 var db = window.openDatabase("test", "1.0", "Test DB", 1000000);
 var InVal = window.localStorage.getItem(get_in_str());
 if (InVal != null){
 	D = new Date(InVal);
 	console.log(D);
-	document.getElementById('in').innerHTML = get_time_str_without_sec2(D);	
+	document.getElementById('in').innerHTML = "<br/><br/><br/><u>Check In </u><br/>"+get_time_str_without_sec2(D);	
 }
 var OutVal = window.localStorage.getItem(get_out_str());
 if (OutVal != null){
 	D = new Date(OutVal);
 	console.log(D);
-	document.getElementById('out').innerHTML = get_time_str_without_sec2(D);	
+	document.getElementById('out').innerHTML = "<br/><br/><br/><u>Check Out </u><br/>"+get_time_str_without_sec2(D);	
 }
 var TimeCompleted = window.localStorage.getItem(get_total_time_str());
 console.log(TimeCompleted);
 if(TimeCompleted!=null){
-	document.getElementById('time-done').innerHTML = get_time_from_msec(TimeCompleted);		
+	document.getElementById('time-done').innerHTML = "<br/><u>Today's Time </u><br/>"+get_time_from_msec(TimeCompleted);		
 }
 else{
-	document.getElementById('time-done').innerHTML = get_time_from_msec(0);	
+	document.getElementById('time-done').innerHTML = "<br/><u>Today's Time </u><br/>"+get_time_from_msec(0);	
 }
 var WeekTime = window.localStorage.getItem(get_week_str());
 console.log(WeekTime);
 if(WeekTime!=null){
-	document.getElementById('time-left').innerHTML = get_time_from_msec(WeekTime);		
+	document.getElementById('time-left').innerHTML = "<br/>"+get_time_from_msec(WeekTime);		
 }
 else{
-	document.getElementById('time-left').innerHTML = get_time_from_msec(get_total_time());	
+	document.getElementById('time-left').innerHTML = "<br/>"+get_time_from_msec(get_total_time());	
 }
 }
 function get_time_from_msec(msec){	
@@ -110,7 +109,7 @@ var key = get_in_str();
 var val = window.localStorage.getItem(key);
 if(val == null){		
 	window.localStorage.setItem(key,get_datetime_object().toString());
-	document.getElementById('in').innerHTML = get_time_str_without_sec();	
+	document.getElementById('in').innerHTML = "<br/><br/><br/><u>Check In </u><br/>"+get_time_str_without_sec();	
 }
 }
 function checkOut(){
@@ -128,15 +127,25 @@ if(val == null){
 	var diff = get_datetime_object().getTime() - ND.getTime();
 	console.log("Diff",diff,get_datetime_object(),InVal);
 	window.localStorage.setItem(get_total_time_str(),diff);			
-	document.getElementById('out').innerHTML = get_time_str_without_sec();
-	document.getElementById('time-done').innerHTML = get_time_from_msec(diff);
+	document.getElementById('out').innerHTML = "<br/><br/><br/><u>Check Out </u><br/>"+get_time_str_without_sec();
+	document.getElementById('time-done').innerHTML = "<br/><u>Today's Time </u><br/>"+get_time_from_msec(diff);
 	var WeekTime = window.localStorage.getItem(get_week_str());
 	if(WeekTime == null)
 		var time = get_total_time();
 	else
 		var time = WeekTime;
 	window.localStorage.setItem(get_week_str(),time-diff);
-	document.getElementById('time-left').innerHTML = get_time_from_msec(time-diff);
+	document.getElementById('time-left').innerHTML = "<br/>"+get_time_from_msec(time-diff);
 	}
 }
+}
+function ResetApp(){
+	window.localStorage.clear();
+	document.getElementById('in').innerHTML = "<button class = 'buttonNew' onclick = 'checkIn()' style = 'background-color: #0288D1;'><b>Check In </b></button>";
+	document.getElementById('out').innerHTML = "<button class = 'buttonNew' onclick = 'checkOut()' style = 'background-color: #D32F2F;'><b>Check Out</b></button>";	
+	document.getElementById('time-done').innerHTML = "<br/><u>Today's Time </u><br/>"+get_time_from_msec(0);
+	document.getElementById('time-left').innerHTML = "<br/>"+get_time_from_msec(get_total_time());
+}
+function ShowDevInfo(){
+	alert("App Developed By: Cyatrosi\nDate: 14/11/2018\nDesc: Keep Track of your office work hours.\nJust Check in and Check out when you punch and \nLet the app do the math.");
 }
