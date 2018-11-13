7052421ee52a7c6986b20ff902555a31f7874406
Nilesh Saxena <dragonair2311@gmail.com>
2018-11-13 22:36:37 +0530

    FC

---
+++ www/js/index.js
@@ -0,0 +1,18 @@
+function setLocal(val){
+	localStorage.mode = val;
+}
+function init(){
+var val = window.localStorage.getItem('nilesh');
+document.getElementById('time-left').innerHTML = val;
+if(val == null)
+	window.localStorage.setItem('nilesh','saxena');
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1;
+var yyyy = today.getFullYear();
+var hh = today.getHours();
+var mm = today.getMinutes();
+var sec = today.getSeconds();
+console.log(dd,mm,yyyy,hh,mm,sec);
+var db = window.openDatabase("test", "1.0", "Test DB", 1000000);
+}
--- www/js/‌index.js
+++
@@ -1,3 +0,0 @@
-function setLocal(val){
-	localStorage.mode = val;
-}
