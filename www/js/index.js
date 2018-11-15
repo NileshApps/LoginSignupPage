c7a262aa30c5bb3e50b51d1ddc28f4d0055a6461
Nilesh Saxena <dragonair2311@gmail.com>
2018-11-15 22:04:20 +0530

    RC

--- www/js/index.js
+++ www/js/index.js
@@ -6,7 +6,7 @@ Date.prototype.getWeek = function() {
     return Math.ceil((((this - dt) / 86400000) + dt.getDay()+1)/7);
 };
 function init(){
-window.localStorage.clear();
+//window.localStorage.clear();
 var NewD = new Date();
 var DayCode = NewD.getDay();
 if(DayCode>=1 && DayCode<=5){
